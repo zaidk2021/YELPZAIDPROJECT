1041dd62291c134ea91e109068be98d5e269610b
zaidk2021 <kzaidnba@gmail.com>
2023-05-15 19:09:45 +0530

    second commit

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
-app.use(xss());//use to sanitize html
+app.use(xss);//use to sanitize html
 
 app.use(express.static(path.join(__dirname,'public')))
 const secret=process.env.SECRET || 'thisshouldbeabettersecret!';
